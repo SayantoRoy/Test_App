32dbc1248c8368024866b5d65550580278df5fe9
Sayanto Roy <sroy.sayanto@gmail.com>
2020-09-21 22:50:52 +0530

    Add project files.

---
+++ Test_App/Controllers/testController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_App.Models;
+
+namespace Test_App.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            testDbEntities testDb = new testDbEntities();
+            List<tbl_emps> emps = testDb.tbl_emps.ToList();
+
+            return View(emps);
+        }
+
+        public ActionResult MainDashboard()
+        {
+            testDbEntities testDb = new testDbEntities();
+            List<tbl_emps> emps = testDb.tbl_emps.ToList();
+            return View(emps);
+        }
+    }
+}
